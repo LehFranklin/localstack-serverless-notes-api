0bab5ff040800595a831c97bd4cacdc784188c1e
leh Dzerinyuy Franklin <lehfranklin9@gmail.com>
2026-09-24 15:49:27 +0300

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -16,3 +16,17 @@ resource "aws_dynamodb_table" "notes" {
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.notes.name
 }
+resource "aws_dynamodb_table" "notes" {
+  name         = "notes"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  server_side_encryption {
+    enabled = true
+  }
+}
